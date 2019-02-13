93aca6c33d5e4652e154e7bf8a257a7af45fd113
Jesse Squires <jessesquires@users.noreply.github.com>
2019-02-12 22:38:33 -0800

    Update Contents.swift

--- source/never_absurd.playground/Contents.swift
+++ source/never_absurd.playground/Contents.swift
@@ -1,6 +1,7 @@
 //
 // Sources / References:
 //
+// Point-Free
 // - https://twitter.com/pteasima/status/978325590397906944
 // - https://www.pointfree.co
 // - https://github.com/apple/swift-evolution/blob/master/proposals/0215-conform-never-to-hashable-and-equatable.md
